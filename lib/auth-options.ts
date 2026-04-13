c07f31a5544ccb4d3458aabd2da8070e723a83f6
Jason <jason@made180.com>
2026-04-12 23:17:53 -0400

    fix: check both NEXTAUTH_SECRET and AUTH_SECRET

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -12,7 +12,7 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
 
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
 
     session: {
         strategy: 'jwt',
